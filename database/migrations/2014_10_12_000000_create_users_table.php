8efa6daae7f598a0f4a34fbb8ba2f21356aa9aa4
Ratthee Jar <me@dnratthee.me>
2019-11-30 05:36:24 +0700

    ui Bootstrap - add auth

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,9 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email')->unique()->charset('utf8')->collation('utf8_unicode_ci');
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->charset('utf8')->collation('utf8_unicode_ci');
             $table->rememberToken();
             $table->timestamps();
         });
--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -14,7 +14,7 @@ class CreatePasswordResetsTable extends Migration
     public function up()
     {
         Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
+            $table->string('email')->index()->charset('utf8')->collation('utf8_unicode_ci');
             $table->string('token');
             $table->timestamp('created_at')->nullable();
         });
